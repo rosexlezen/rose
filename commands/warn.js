c02d377f68faea7e80967bb3d01dc9fe534dda65
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-05 15:57:07 +0100

    added a bunch of stuff
    - warn embed
    - json file
    - edited text

--- commands/warn.js
+++ commands/warn.js
@@ -1,8 +1,11 @@
 const discord = require("discord.js");
+const fs = require("fs");
+const warns = JSON.parse(fs.readFileSync("./warnings.json", "utf8"));
 
 module.exports.run = async(client, message, args) => {
 
-    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry but you cannot use this command!");
+    // Permissions
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry but you cannot execute this command!");
     if (!args[0]) return message.reply("you need to specify an user.");
     if (!args[1]) return message.reply("you need to specify a valid reason!");
     if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("I cannot execute this command! Please give me ``Kick Permissions``");
@@ -12,6 +15,36 @@ module.exports.run = async(client, message, args) => {
     var reason = args.slice(1).join(" ");
 
     if(warnUser.hasPermission("ADMINISTRATOR")) return message.reply("you cannot warn this user.");
+
+    // Command
+
+    if(!warns[warnUser.id]) warns[warnUser.id] = {
+        warns: 0
+    };
+
+    warns[warnUser.id].warns++; 
+
+    fs.writeFile("./warnings.json", JSON.stringify(warns), (err) =>{
+        if(err) console.log(err);
+    });
+
+    var warnEmbed = new discord.MessageEmbed()
+    .setColor('#d105ff')
+	.setTitle('Warned User!')
+	.setDescription('Warn information:')
+	.setThumbnail('https://cdn.discordapp.com/attachments/773879672676548609/773880508743680000/aiudha.jpg')
+    .addFields(
+        {name: `Warned By:`, value: `${message.author}`},
+        {name: `User Warned:`, value: `${warnUser} (${warnUser.id})`},
+        {name: `Reason:`, value: `${reason}`},
+        {name: `Total Warns:`, value: warns[warnUser.id].warn},
+        )
+	.setTimestamp()
+    .setFooter('creator: Lezen#3283', 'https://cdn.discordapp.com/attachments/773879672676548609/773880029859020820/awdaw.jpg');
+    
+    var channel = message.member.guild.channels.cache.find(c => c.name === 'logs')
+    if(!channel) return message.reply(`please create a "logs" channel to see warn messages.\n ${warnEmbed}`)
+    channel.send(warnEmbed);
 
 } 
 
