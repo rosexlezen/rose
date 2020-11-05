fa439581e75c2a1b4210b3f88d692b07116b546f
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-05 21:35:21 +0100

    slowly going back to a normal message :D

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ client.on("message", async message =>{
     .setColor("#d105ff");
 
     var channel = message.member.guild.channels.cache.find(c => c.name === 'logs');
-    if(!channel) return message.channel.send('``This shouldnt here..``');
+    if(!channel) return;
     channel.send(embedMsg);
     });
 });
