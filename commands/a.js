a28f46db6a5f8a62cae04eda6033abcc751607d0
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-05 21:28:13 +0100

    b

--- commands/a.js
+++ commands/a.js
@@ -14,7 +14,7 @@ module.exports.run = async(client, message, args) => {
 
         const channel = client.channels.cache.find(c => c.name === 'logs');
         if(!channel) return;
-        channel.send(embedMsg);
+        channel.send(`test`);
 
     }
 
