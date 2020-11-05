0b47ac03835308df516b272956550b69227eab1f
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-05 15:27:22 +0100

    created warn command
    not finished yet

---
+++ commands/warn.js
@@ -0,0 +1,20 @@
+const discord = require("discord.js");
+
+module.exports.run = async(client, message, args) => {
+
+    if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry but you cannot use this command!");
+    if (!args[0]) return message.reply("you need to specify an user.");
+    if (!args[1]) return message.reply("you need to specify a valid reason!");
+    if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("I cannot find the KICK permission to execute this command!");
+
+    var warnUser = message.guild.member(message.mentions.user.first() || message.guild.member.get(args[0]));
+    if (!warnUser) return message.reply("I cannot find the user.");
+    var reason = args.slice(1).join(" ");
+
+    if(warnUser.hasPermission("MANAGE_MESSAGES")) return message.reply("you cannot warn this user.");
+
+} 
+
+module.exports.help = {
+    name: "warn"
+}
