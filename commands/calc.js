a0b152e7b95179139f5caff320a902716cc68283
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-05 22:16:52 +0100

    added calc command

---
+++ commands/calc.js
@@ -0,0 +1,25 @@
+const math = require("mathjs");
+const discord = require("discord.js");
+
+module.exports.run = async(client, message, args) => {
+
+    if(!args[0]) return message.reply("please input a calculation.");
+    let resp;
+    try {
+        resp = math.eval(args.join(' '));
+    } catch (e) {
+        return message.reply("please input a valid calculation.")
+    }
+
+    const calcEmbed = new discord.MessageEmbed()
+    .setColor(`d105ff`)
+    .setTitle(`Math Calculation`)
+    .addField(`Input`, `js\n${args.join(' ')}`)
+    .addField('Output', `js\n${resp}`)
+
+    message.channel.send(calcEmbed)
+} 
+
+module.exports.help = {
+    name: "calc"
+}
