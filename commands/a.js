299b4abbdc88b31a282f26273a409d48b8b77716
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-05 21:19:51 +0100

    you're a furry

--- commands/a.js
+++ commands/a.js
@@ -2,16 +2,9 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
 
-     var response = `**Message ID:** ${messageDeleted.id}\n**Message Author:** ${message.author}\n**Channel:** ${messageDeleted.channel}\n**Message:** ${content}`;
-     var embedMsg = new discord.MessageEmbed()
-     .setAuthor(`${messageDeleted.author.tag} ( ${messageDeleted.author.id} )`, `${messageDeleted.author.avatarURL({ size:4096 })}`)
-     .setDescription(response)
-    .setTimestamp()
-     .setColor("#d105ff");
-
     const channel = client.channels.cache.find(c => c.name === 'logs');
-     if(!channel) return;
-     channel.send(embedMsg);
+    if(!channel) return;
+    channel.send(`meow`);
 
     }
 
