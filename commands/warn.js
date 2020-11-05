690e8bbfb5284cef98dd0337f3d769f4e0a37533
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-05 15:36:32 +0100

    fixed error
    user to users

--- commands/warn.js
+++ commands/warn.js
@@ -7,7 +7,7 @@ module.exports.run = async(client, message, args) => {
     if (!args[1]) return message.reply("you need to specify a valid reason!");
     if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("I cannot execute this command! Please give me ``Kick Permissions``");
 
-    var warnUser = message.guild.member(message.mentions.user.first() || message.guild.member.get(args[0]));
+    var warnUser = message.guild.member(message.mentions.users.first() || message.guild.member.get(args[0]));
     if (!warnUser) return message.reply("I cannot find the user.");
     var reason = args.slice(1).join(" ");
 
