cc3dc7a445100a74ab0129688e9059aa252c180f
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-05 15:33:45 +0100

    warn command perms
    updated text and required permissions

--- commands/warn.js
+++ commands/warn.js
@@ -5,13 +5,13 @@ module.exports.run = async(client, message, args) => {
     if (!message.member.hasPermission("KICK_MEMBERS")) return message.reply("sorry but you cannot use this command!");
     if (!args[0]) return message.reply("you need to specify an user.");
     if (!args[1]) return message.reply("you need to specify a valid reason!");
-    if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("I cannot find the KICK permission to execute this command!");
+    if (!message.guild.me.hasPermission("KICK_MEMBERS")) return message.reply("I cannot execute this command! Please give me ``Kick Permissions``");
 
     var warnUser = message.guild.member(message.mentions.user.first() || message.guild.member.get(args[0]));
     if (!warnUser) return message.reply("I cannot find the user.");
     var reason = args.slice(1).join(" ");
 
-    if(warnUser.hasPermission("MANAGE_MESSAGES")) return message.reply("you cannot warn this user.");
+    if(warnUser.hasPermission("MANAGE_MESSAGES") || warnUser.hasPermission("ADMINISTRATOR")) return message.reply("you cannot warn this user.");
 
 } 
 
