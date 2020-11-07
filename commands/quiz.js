bb851cb1596bfd673636210ec4d895a4de271772
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-07 17:23:35 +0100

    a
    a

--- commands/quiz.js
+++ commands/quiz.js
@@ -156,6 +156,28 @@ message.channel.send("**―――――――――――――――**\nChoose 
     }
 });
 
+
+// 
+//
+// Reaction Message
+//
+//
+
+async function reactionMessage(message, author, time, reactions) {
+
+
+    time *= 1000;
+
+    for(const reaction of reactions) {
+        await message.react(reaction);
+    }
+
+    const filter = (reaction, user) => reactions.includes(reaction.emoji.name) && user.id === author.id;
+
+    return message.awaitReactions(filter, {max: 1, time: time}).then(collected => collected.first() && collected.first().emoji.name);
+
+}
+
 }
 module.exports.help = {
     name: "quiz"
