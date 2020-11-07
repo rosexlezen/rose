97076386ede8f86c7359f70c49f737059d92394e
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-07 09:13:52 +0100

    a
    a

--- commands/stats.js
+++ commands/stats.js
@@ -3,6 +3,8 @@ let fetch = require('node-fetch');
 
 module.exports.run = async(client, message, args) => {
 
+    message.channel.send("hey", {files: ["https://cdn.discordapp.com/attachments/773879672676548609/774542590925733898/2020-11-04_14.19.15.png?size=2048"]});
+
     // Getting the API
     if(!args[0]) return message.reply('please provide a username.');
     fetch(`https://api.plancke.io/hypixel/v1/player?player=${args[0]}`)
@@ -13,8 +15,7 @@ module.exports.run = async(client, message, args) => {
         var statsEmbed = new discord.MessageEmbed()
         .setColor('#d105ff')
 	    .setDescription('Statistics information:')
-	    .setImage("https://cdn.discordapp.com/attachments/773879672676548609/774542590925733898/2020-11-04_14.19.15.png?size=2048")
-        .setThumbnail("https://cdn.discordapp.com/attachments/773879672676548609/774542590925733898/2020-11-04_14.19.15.png?size=2048")
+        //.setThumbnail("https://cdn.discordapp.com/attachments/773879672676548609/774542590925733898/2020-11-04_14.19.15.png?size=2048")
         .addFields(
         {name: `Player:`, value: `${record.displayname}`},
         {name: `Statistics:`, value: `:sparkles: Network Experience: ${record.networkExp}`},
@@ -28,7 +29,7 @@ module.exports.run = async(client, message, args) => {
         .setFooter(`Executed by: ${message.author.tag}`, `${message.author.avatarURL()}`);
 
         // Sending stats
-        return message.channel.send(statsEmbed).then(d => d.delete({timeout: 15000}));
+        return message.channel.send(statsEmbed), {files: ["https://cdn.discordapp.com/attachments/773879672676548609/774542590925733898/2020-11-04_14.19.15.png?size=2048"]};
         })
     .catch(err => console.log(err));
     }
