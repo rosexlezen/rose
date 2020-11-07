b92cb75a9d68ca4df6a393940f342d0fc0b15f77
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-07 17:52:27 +0100

    a
    a

--- commands/kev.js
+++ commands/kev.js
@@ -69,12 +69,17 @@ module.exports.run = async(client, message, args) => {
         //
 
     message.channel.send(GeneralEmbed).then(async msg => {
-    var emoji = await reactionMessage(msg, message.author, ["⏩"]);
+    var emoji = await reactionMessage(msg, message.author, 300, ["⏪", "⏩"]);
     
-    if (emoji === "⏩") {
+    if (emoji === "⏪") {
         message.channel.send(SkyWarsEmbed).then(async msg => {
-            var emoji = await reactionMessage(msg, message.author, ["⏪", "⏩"]);
+            var emoji = await reactionMessage(msg, message.author, 300, ["⏪", "⏩"]);
+        });
+    }
 
+    if (emoji === "⏩") {
+        message.channel.send(BedWarsEmbed).then(async msg => {
+            var emoji = await reactionMessage(msg, message.author, 300, ["⏪", "⏩"]);
         });
     }
 });
