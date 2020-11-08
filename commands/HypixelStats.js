5ba5c944dd0df288f4ca8f1a13cd743636fe5ba0
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-08 13:59:38 +0100

    Update HypixelStats.js

--- commands/HypixelStats.js
+++ commands/HypixelStats.js
@@ -4,9 +4,12 @@ let fetch = require('node-fetch');
 
 module.exports.run = async(client, message, args) => {
 
-    
-
-    if(typeof args[0] === null) return message.channel.send("``Player not found!``")
+    function undifined() {
+        return message.channel.send("``Player not found!``");
+    }
+    // if(typeof undifined === undifined) {
+    //     return message.channel.send("``Player not found!``");
+    // }
     if(!args[0]) return message.reply('please provide a username.');
     fetch(`https://api.plancke.io/hypixel/v1/player?player=${args[0]}`)
     .then(res => res.json()) 
