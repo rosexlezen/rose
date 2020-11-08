405b26060fad2eb365145b7ed2322e3f2ac3616e
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-08 13:38:57 +0100

    Update HypixelStats.js

--- commands/HypixelStats.js
+++ commands/HypixelStats.js
@@ -92,7 +92,8 @@ module.exports.run = async(client, message, args) => {
 	    .setDescription("⏪ = Skywars ⏩ = N/A")
         .addFields(
         {name: `Player:`, value: `${record._custom.names.stripped.name}`, inline: true},
-        //{name: `Extra Information:`, value: `Level: ${bedwarsLevel}⭐ ( ${record.stats.bedwars.Experience} ) \nWinstreak: ${record.stats.bedwars.winstreak}`, inline: true},
+        {name: `Extra Information:`, value: `Level: ${bedwarsLevel}⭐ ( a ) \nWinstreak: ${record.stats.bedwars.winstreak}`, inline: true},
+        //${record.stats.Bedwars.Experience}
         )
 	    .setTimestamp()
         .setFooter(`Executed by: ${message.author.tag}`, `${message.author.avatarURL()}`);
