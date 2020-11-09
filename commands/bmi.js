65fe63d39892eb446fc6165e56a473b162ebeb4b
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-09 19:21:33 +0100

    Update bmi.js

--- commands/bmi.js
+++ commands/bmi.js
@@ -2,11 +2,16 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
 
+    collector.on('collect', m => {
     message.channel.send(`How tall are you in meters?`);
+    if (args[0] === isNaN) return message.reply("please input an int!");
     var length = args[-1];
-
+});
+    collector.on('collect', m => {
     message.channel.send(`How much do you weight?`);
+    if (args[0] === isNaN) return message.reply("please input an int!");
     var weight = args[-1];
+})
 
     var lengthSquare = length ** 2;
     var BMI = weight / lengthSquare * 10000;
