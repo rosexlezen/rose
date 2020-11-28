cfd16f6af5f10db91c1dc4e0ee738649444e90bf
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-29 00:12:21 +0100

    Update typeracer.js

--- commands/typeracer.js
+++ commands/typeracer.js
@@ -2,7 +2,6 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
 
-    var timeout = Math.floor((Math.random() * 10) + 1);
     var sentences = [
         "https://discord.com/channels/770516707370467328/770574971818344448/782377489636917288",
         "a"
@@ -10,11 +9,10 @@ module.exports.run = async(client, message, args) => {
     var type = sentences[Math.floor(Math.random() * sentences.length)];
 
     message.channel.send("Typeracer will start in 1-10 seconds, be ready!").then(d => d.delete({timeout: 5000}));
-    message.channel.send(`(Timeout time = timeout)`);
 
     setTimeout(function(){
         message.channel.send(`Message will automatically be deleted in 20 seconds! \n${type}`).then(d => d.delete({timeout: 20000})); 
-    }, timeout);
+    }, Math.floor((Math.random() * 10) + 1));
 
 } 
 
