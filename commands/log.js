a728d7aeee21317dbe9474c15a854d0c5102f930
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-29 01:51:55 +0100

    a

--- commands/log.js
+++ commands/log.js
@@ -2,11 +2,10 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
 
-    if(message.author.id === "esmee#9999") return console.log("----------------------\n----------------------\n----------------------\n");
+    if(message.author.id != "esmee#9999") return message.reply(`You cannot do this.`).then(d => d.delete({timeout: 5000}));
     else {
-        return;
+    console.log("----------------------\n----------------------\n----------------------\n")
     }
-
 } 
 
 module.exports.help = {
--- commands/typeracer.js
+++ commands/typeracer.js
@@ -13,13 +13,14 @@ module.exports.run = async(client, message, args) => {
     var min = 5, max = 10;
     var rand = Math.floor(Math.random() * (max - min + 1) + min);
 
-    message.channel.send("Typeracer will start in 1-10 seconds, be ready!").then(d => d.delete({timeout: 5000}));
+    message.channel.send("Typeracer will start in 1-10 seconds, be ready!").then(d => d.delete({timeout: 15000}));
 
       setTimeout(() => {
         message.channel.send(`**This message will delete in 20 seconds!**\nFirst who types "${type}" wins!\n*This message was sent in ${rand} seconds!*`).then(d => d.delete({timeout: 20000}));
       }, rand * 1000);
 
-    //   var guessed = message.author; 
+       var guessed = message.author;
+       message.channel.send(guessed); 
 
     //   // Array 1
     //   if(type === "https://media.discordapp.net/attachments/770574971818344448/782399042981724221/unknown.png") {
