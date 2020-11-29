d132ff353c3e6e87250360e09ad38e7cff50c3bc
esmee <73444870+reallycute@users.noreply.github.com>
2020-11-29 02:07:35 +0100

    Update typeracer.js

--- commands/typeracer.js
+++ commands/typeracer.js
@@ -19,59 +19,56 @@ module.exports.run = async(client, message, args) => {
         message.channel.send(`**This message will delete in 30 seconds!**\nFirst who types "${type}" wins!\n*This message was sent in ${rand} seconds!*`).then(d => d.delete({timeout: 30000}));
       }, rand * 1000);
 
-    // Array 1
-    if(type === "https://media.discordapp.net/attachments/770574971818344448/782399042981724221/unknown.png") {
+       // Array 1
+       if(type === "https://media.discordapp.net/attachments/770574971818344448/782399042981724221/unknown.png") {
         message.channel.awaitMessages({max: 1, time: 30000}).then(collected => {
-            message.channel.send("b");
                   if(collected.first().content.toLowerCase() == 'monkey see, monkey do') {
-                          message.reply(`a`);
+                          message.reply(`${guessed} got it first!`);
                   }
                   else
                   message.reply('Operation canceled.').then(d => d.delete({timeout: 5000}));      
                   }).catch(() => {
                   message.reply('No answer after 30 seconds, operation canceled.').then(d => d.delete({timeout: 5000}));
               });
-      } 
-    // Array 2
-//     else if(type === "https://media.discordapp.net/attachments/770574971818344448/782377489464295484/unknown.png") {
-//       message.channel.awaitMessages({max: 1, time: 30000}).then(collected => {
-//                 if(collected.first().content.toLowerCase() == 'joshua is the greatest person of all time.') {
-//                         message.reply(`${guessed} got it first!`);
-//                 }
-//                 else
-//                 message.reply('Operation canceled.').then(d => d.delete({timeout: 5000}));      
-//                 }).catch(() => {
-//                 message.reply('No answer after 30 seconds, operation canceled.').then(d => d.delete({timeout: 5000}));
-//             });
-//     }   
-//     // Array 3
-//     else if(type === "https://media.discordapp.net/attachments/770574971818344448/782398767826599997/unknown.png") {
-//         message.channel.awaitMessages({max: 1, time: 30000}).then(collected => {
-//                   if(collected.first().content.toLowerCase() == "i ate food now i'm tired") {
-//                           message.reply(`${guessed} got it first!`);
-//                   }
-//                   else
-//                   message.reply('Operation canceled.').then(d => d.delete({timeout: 5000}));      
-//                   }).catch(() => {
-//                   message.reply('No answer after 30 seconds, operation canceled.').then(d => d.delete({timeout: 5000}));
-//               });
-//       }   
-//       // Array 4
-//   else if(type === "https://media.discordapp.net/attachments/770574971818344448/782398900195033119/unknown.png") {
-//     message.channel.awaitMessages({max: 1, time: 30000}).then(collected => {
-//               if(collected.first().content.toLowerCase() == "i'm exhausted all day long") {
-//                       message.reply(`${guessed} got it first!`);
-//               }
-//               else
-//               message.reply('Operation canceled.').then(d => d.delete({timeout: 5000}));      
-//               }).catch(() => {
-//               message.reply('No answer after 30 seconds, operation canceled.').then(d => d.delete({timeout: 5000}));
-//           });
-//   } else {
-//       message.channel.send("Something went wrong...");
-//   }   
+      }
+  else if(type === "https://media.discordapp.net/attachments/770574971818344448/782377489464295484/unknown.png") {
+        message.channel.awaitMessages({max: 1, time: 30000}).then(collected => {
+                  if(collected.first().content.toLowerCase() == 'joshua is the greatest person of all time.') {
+                          message.reply(`${guessed} got it first!`);
+                  }
+                  else
+                  message.reply('Operation canceled.').then(d => d.delete({timeout: 5000}));      
+                  }).catch(() => {
+                  message.reply('No answer after 30 seconds, operation canceled.').then(d => d.delete({timeout: 5000}));
+              });
+      }   
+      // Array 3
+      else if(type === "https://media.discordapp.net/attachments/770574971818344448/782398767826599997/unknown.png") {
+          message.channel.awaitMessages({max: 1, time: 30000}).then(collected => {
+                    if(collected.first().content.toLowerCase() == "i ate food now i'm tired") {
+                            message.reply(`${guessed} got it first!`);
+                    }
+                    else
+                    message.reply('Operation canceled.').then(d => d.delete({timeout: 5000}));      
+                    }).catch(() => {
+                    message.reply('No answer after 30 seconds, operation canceled.').then(d => d.delete({timeout: 5000}));
+                });
+        }   
+        // Array 4
+    else if(type === "https://media.discordapp.net/attachments/770574971818344448/782398900195033119/unknown.png") {
+      message.channel.awaitMessages({max: 1, time: 30000}).then(collected => {
+                if(collected.first().content.toLowerCase() == "i'm exhausted all day long") {
+                        message.reply(`${guessed} got it first!`);
+                }
+                else
+                message.reply('Operation canceled.').then(d => d.delete({timeout: 5000}));      
+                }).catch(() => {
+                message.reply('No answer after 30 seconds, operation canceled.').then(d => d.delete({timeout: 5000}));
+            });
+    } else {
+        message.channel.send("Something went wrong...");
+    }   
 }
-
 
 module.exports.help = {
     name: "typeracer"
