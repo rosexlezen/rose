01ee77fc3b848ce4a87dcb7b10df7724bb4622ab
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-18 15:37:31 +0100

    Update uhu.js

--- commands/uhu.js
+++ commands/uhu.js
@@ -9,10 +9,12 @@ module.exports.run = async(client, message, args) => {
       setTimeout(function(){
         msg.edit(`Fetching ${args[0]}'s Dox..`);
       }, 1000)
+
       .then((msg) => {
-        setTimeout(() => {
-            msg.edit(`Fetching ${args[0]}'s Dox.`);
+        setTimeout(function(){
+            msg.edit(`Fetching ${args[0]}'s Dox...`);
         }, 2000)
+
         .then((msg) => {
             msg.edit(`hi`)
         });
