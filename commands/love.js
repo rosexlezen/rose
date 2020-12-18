f64bc3b2bc548f789830c1cbaf603bca9ce9ff6a
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-18 15:06:01 +0100

    aswda

--- commands/love.js
+++ commands/love.js
@@ -3,14 +3,27 @@ const discord = require("discord.js");
 module.exports.run = async(client, message, args) => {
 
 if (!args[0]) return message.reply("whose love do you want to check?");
-    else if (!message.content.includes("@")) return message.reply("please tag the person you want check.");
-    else if (!message.mentions.members.first()) return message.reply("you can't use the lovemeter on roles!");
+
+else if (!args[1] || !args[2]) return message.reply("please provide the names you want love check.");
 
     var result = Math.ceil(Math.random() * 100);
-    
-    let user = message.mentions.users.first(),member;
     if (user) member = message.guild.member(user);
-    message.channel.send(`${message.author} and ${user} are ${result}% in love`);
+    message.channel.send(`${message.author}, ${args[1]} and ${args[2]} are ${result}% in love`);
+
+
+//
+//
+// OLD VERSION
+//
+//
+//     else if (!message.content.includes("@")) return message.reply("please tag the person you want check.");
+//     else if (!message.mentions.members.first()) return message.reply("you can't use the lovemeter on roles!");
+
+//     var result = Math.ceil(Math.random() * 100);
+    
+//     let user = message.mentions.users.first(),member;
+//     if (user) member = message.guild.member(user);
+//     message.channel.send(`${message.author} and ${user} are ${result}% in love`);
 
 }
 
