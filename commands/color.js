b3fc16c26323463650ad5b0a70abd8f22e95863a
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-18 14:47:42 +0100

    a

--- commands/color.js
+++ commands/color.js
@@ -2,8 +2,8 @@ const Discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
 
-
-    message.channel.send(":black: Black » &0 \n:dark_blue: Dark Blue » &1 \n:dark_green: Dark Green » &2 \n:dark_turquoise: Dark Turquoise » &3\n:dark_red: Dark Red » &4 \n:purple: Purple » &5 \n:gold: Gold » &6 \n:gray: Light Gray » &7 \n:dark_gray: Dark Gray » &8 \n:blue: Blue » &9 \n:green: Green » &a \n:turqiouse: Turqiouse » &b \n:red: Red » &c \n:pink: Pink » &d \n:yellow: Yellow » &e \n:white: White » &f");
+    message.channel.send("Coming Soon!");
+    //message.channel.send(":black: Black » &0 \n:dark_blue: Dark Blue » &1 \n:dark_green: Dark Green » &2 \n:dark_turquoise: Dark Turquoise » &3\n:dark_red: Dark Red » &4 \n:purple: Purple » &5 \n:gold: Gold » &6 \n:gray: Light Gray » &7 \n:dark_gray: Dark Gray » &8 \n:blue: Blue » &9 \n:green: Green » &a \n:turqiouse: Turqiouse » &b \n:red: Red » &c \n:pink: Pink » &d \n:yellow: Yellow » &e \n:white: White » &f");
 
 }
 
--- commands/hex.js
+++ commands/hex.js
@@ -5,12 +5,12 @@ module.exports.run = async(client, message, args) => {
     if(!args[0]) return message.reply(`Please provide a # color (example: #000000).`);
     if(!isNaN) return message.reply(`Please provide a # color (example: #000000)`);
 
-    var color = args[1]
+    var color = args[0]
 
     const hex = new Discord.MessageEmbed()
-    .setColor("#" + color)
+    .setColor(color)
     .setTitle(color)
-    
+
     message.channel.send(hex);
 
 }
