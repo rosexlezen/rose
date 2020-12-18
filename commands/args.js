c2075b465eea4c09f9e25475054685e2b75d218c
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-18 15:14:59 +0100

    awd

--- commands/args.js
+++ commands/args.js
@@ -3,11 +3,12 @@ const discord = require("discord.js");
 module.exports.run = async(client, message, args) => {
 
     if(!args[0]) return message.reply(`you have not given at least me one argument!`)
-    if(args[1]) return message.channel.send(`0: ${args[0]}`);
-    if(args[1]) return message.channel.send(`0: ${args[0]}, 1: ${args[1]}`);
-    if(args[1]) return message.channel.send(`0: ${args[0]}, 1: ${args[1]}, 2: ${args[2]}`);
-    if(args[1]) return message.channel.send(`0: ${args[0]}, 1: ${args[1]}, 2: ${args[2]}, 3: ${args[3]}`);
-    if(args[1]) return message.channel.send(`0: ${args[0]}, 1: ${args[1]}, 2: ${args[2]}, 3: ${args[3]}, 4: ${args[4]}`);
+    else if(args[1]) return message.channel.send(`0: ${args[0]}`);
+    else if(args[1]) return message.channel.send(`0: ${args[0]}, 1: ${args[1]}`);
+    else if(args[1]) return message.channel.send(`0: ${args[0]}, 1: ${args[1]}, 2: ${args[2]}`);
+    else if(args[1]) return message.channel.send(`0: ${args[0]}, 1: ${args[1]}, 2: ${args[2]}, 3: ${args[3]}`);
+    else if(args[1]) return message.channel.send(`0: ${args[0]}, 1: ${args[1]}, 2: ${args[2]}, 3: ${args[3]}, 4: ${args[4]}`);
+    else return message.reply(`you are a maniac!`);
 
 } 
 
