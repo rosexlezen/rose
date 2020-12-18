f056f2055dcc7756d00bc1e0d266727e389fb588
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-18 14:11:41 +0100

    Update av.js

--- commands/av.js
+++ commands/av.js
@@ -2,27 +2,16 @@ const Discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
 
-    const user = message.mentions.users.first();
+    const user = message.mentions.users.first() || message.author;
     let pfp = user.avatarURL();
-
-    if(!args[0]) {
-        const avatarEmbed = new Discord.MessageEmbed()
-        .setColor("#ffd4fa")
-        .setTitle(`**${user.username}**'s Profile Picture:`)
-        .setImage(message.author.avatarURL());
-
-        message.channel.send(avatarEmbed);
-    }
-
-    else {
-        
+    
     const avatarEmbed = new Discord.MessageEmbed()
         .setColor("#ffd4fa")
-        .setTitle(`**${user.username}**'s Profile Picture:`)
-        .setImage(pfp);
+        .setTitle(`**${user}**'s Profile Picture:`)
+        .setImage(pfp)
+        .setThumbnail(message.author.avatarURL())
 
     message.channel.send(avatarEmbed);
-    }
 } 
 
 module.exports.help = {
