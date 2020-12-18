38845ed077af659e83e31662d3c28cdbb62e5fba
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-18 15:33:28 +0100

    Create dox.js

---
+++ commands/dox.js
@@ -0,0 +1,26 @@
+const Discord = require("discord.js");
+
+module.exports.run = async(client, message, args) => {
+    
+    if(!args[0]) return;
+
+    message.channel.send(`Fetching ${args[0]}'s Dox.`)
+    .then((msg)=> {
+      setTimeout(function(){
+        msg.edit(`Fetching ${args[0]}'s Dox..`);
+      }, 1000)
+      .then((msg) => {
+        setTimeout(() => {
+            msg.edit(`Fetching ${args[0]}'s Dox.`);
+        }, 2000)
+        .then((msg) => {
+            msg.edit(`hi`)
+        });
+      });
+    }); 
+
+} 
+
+module.exports.help = {
+    name: "dox"
+}
