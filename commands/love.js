1271e29638f5c8e1c0125d3fd6efc3d4eb6b5eac
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-18 15:24:05 +0100

    awdaw

--- commands/love.js
+++ commands/love.js
@@ -2,12 +2,10 @@ const discord = require("discord.js");
 
 module.exports.run = async(client, message, args) => {
 
-if (!args[0]) return message.reply("whose love do you want to check?");
-else if (!args[1]) return message.reply("please provide the names you want love check.");
-else if (!args[2]) return message.reply("please provide the names you want love check.");
+    if (!args[0]) return message.reply("please provide the names you want love check.");
+    else if (!args[1]) return message.reply("please provide the names you want love check.");
 
     var result = Math.ceil(Math.random() * 100);
-    if (user) member = message.guild.member(user);
     message.channel.send(`${message.author}, ${args[1]} and ${args[2]} are ${result}% in love`);
 
 
