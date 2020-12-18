4fce190b33494bb80ac41c38f5178b862d17dc7a
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-18 13:50:12 +0100

    Update av.js

--- commands/av.js
+++ commands/av.js
@@ -4,10 +4,12 @@ module.exports.run = async(client, message, args) => {
     
     const user = message.mentions.users.first() || message.author;
 
+    var pfp = user.avatar_url;
+
     const avatarEmbed = new Discord.MessageEmbed()
         .setColor("#ffd4fa")
         .setAuthor(user.username)
-        .setImage(user.avatarURL);
+        .setImage(pfp);
 
     message.channel.send(avatarEmbed);
 
