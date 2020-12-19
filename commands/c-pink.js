6e4f420df4c46aa2d8aefdc29d6d425ef6311919
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-20 00:44:51 +0100

    Update c-pink.js

--- commands/c-pink.js
+++ commands/c-pink.js
@@ -4,8 +4,7 @@ module.exports.run = async(client, message, args) => {
 
     if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry but you cannot use this command!");
 
-    var channel = (message.guild.channels.exists('name', reactionroles))
-    if(!channel) return message.reply('```Create a channel called: "reactionroles"```');
+    if (message.guild.channels.cache.find(c => c.name.toLowerCase() != "reactionroles")) return message.reply('```Create a channel called: "reactionroles"```');
 
     // guild.roles.create({ data: { name: 'test', color: ['#f9d4fa'] } });
     // guild.roles.create({ data: { name: 'test1', color: ['#f599f7'] } });
