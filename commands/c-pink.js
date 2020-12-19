28c335ec5d508de165fe1b75712655c46fb04ad8
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-20 00:57:48 +0100

    Update c-pink.js

--- commands/c-pink.js
+++ commands/c-pink.js
@@ -5,16 +5,18 @@ module.exports.run = async(client, message, args) => {
     if (!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Sorry but you cannot use this command!");
     if ((message.guild.channels.cache.find(c => c.name.toLowerCase() === "reactionroles"))) {
 
-    // guild.roles.create({ data: { name: 'test', color: ['#f9d4fa'] } });
-    // guild.roles.create({ data: { name: 'test1', color: ['#f599f7'] } });
+    guild.roles.create({ data: { name: 'test', color: ['#f9d4fa'] } });
+    guild.roles.create({ data: { name: 'test1', color: ['#f599f7'] } });
 
     const picture = new discord.MessageEmbed()
     .setThumbnail('https://cdn.discordapp.com/attachments/770574971818344448/789994438654558228/tenor_1.gif')
 
     const ColorPink = new discord.MessageEmbed()
-    .setColor(color)
-    .setTitle(color)
-    .setURL("https://www.google.com/search?q=hex+color")
+    .setColor("#000000")
+    .addFields(
+        {value: "@test"},
+        {value: "@test1"}
+    )
 
     message.channel.send(picture)
     message.channel.send(ColorPink);
