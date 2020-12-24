17771308e2ce34eca61d842c6a081a515c6a4b4d
esmee <73444870+reallycute@users.noreply.github.com>
2020-12-24 01:51:20 +0100

    Update love.js

--- commands/love.js
+++ commands/love.js
@@ -5,23 +5,14 @@ module.exports.run = async(client, message, args) => {
     if (!args[0]) return message.reply("please provide the names you want love check.");
     else if (!args[1]) return message.reply("please provide the names you want love check.");
 
+    if(arg[0] == "alex" && arg[1] == "dylan") {
+        message.channel.send(`${message.author}, **${args[0]}** and **${args[1]}** are 102% in love`);
+    }
+
     var result = Math.ceil(Math.random() * 100);
     message.channel.send(`${message.author}, **${args[0]}** and **${args[1]}** are ${result}% in love`);
 
 
-//
-//
-// OLD VERSION
-//
-//
-//     else if (!message.content.includes("@")) return message.reply("please tag the person you want check.");
-//     else if (!message.mentions.members.first()) return message.reply("you can't use the lovemeter on roles!");
-
-//     var result = Math.ceil(Math.random() * 100);
-    
-//     let user = message.mentions.users.first(),member;
-//     if (user) member = message.guild.member(user);
-//     message.channel.send(`${message.author} and ${user} are ${result}% in love`);
 
 }
 
